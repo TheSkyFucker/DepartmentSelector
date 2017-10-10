587fd97bbb7ee0e76a701495153ca9b5120e8d01
kino <359084415@qq.com>
2017-10-10 19:49:36 +0800

    Base Finish.

--- DepartmentSelector/DepartmentSelector/DepartmentSelector/DateGenerator.cpp
+++ DepartmentSelector/DepartmentSelector/DepartmentSelector/DateGenerator.cpp
@@ -1,9 +1,69 @@
 ﻿#include "stdafx.h"
 #include "DateGenerator.h"
+#include <set>
+#include <map>
 
+
+void DateGenerator::InitTags() throw()
+{
+    //config
+    const std::string tagsTable[] = {
+        "film", "English", "reading", "music", "dance", "basketball", "chess",
+        "programming", "drawing", "writing", "football"
+    };
+
+    //add
+    std::set<std::string> tagsSet;
+    for (auto aTag : tagsTable)
+    {
+        if (tagsSet.find(aTag) != tagsSet.end())
+        {
+            tagsSet.insert(aTag);
+            m_tags.push_back(aTag);
+        }
+    }
+
+}
+
+std::vector<TimeSegment> DateGenerator::randStudentFreeTimes() throw()
+{
+    //config
+    const std::string DAYS_TABLE[] = { "Sun.", "Mon.", "Tues", "Wed", "Fri", "Sat" };
+    const std::string SEGS_TABLE[] = { "8: 20~10: 00", "10: 20~12: 00", "14: 00~15: 40", "15: 50~17:30", "19: 00~20: 40", "20: 50~22:30" };
+
+    //add
+    std::map
+    for (auto aDay : DAYS_TABLE)
+    {
+        for (auto aSeg : SEGS_TABLE)
+        {
+            if (tagsSet.find(aTag) != tagsSet.end())
+            {
+                tagsSet.insert(aTag);
+                m_tags.push_back(aTag);
+            }
+        }
+    }
+}
+
+void DateGenerator::ChangeTags(std::vector<std::string> tags) throw()
+{
+    m_tags.clear();
+    std::set<std::string> tagsSet;
+    for (auto aTag : tags)
+    {
+        if (tagsSet.find(aTag) != tagsSet.end())
+        {
+            tagsSet.insert(aTag);
+            m_tags.push_back(aTag);
+        }
+    }
+}
 
 DateGenerator::DateGenerator()
 {
+    //Init
+    InitTags();
 }
 
 
--- DepartmentSelector/DepartmentSelector/DepartmentSelector/DateGenerator.h
+++ DepartmentSelector/DepartmentSelector/DepartmentSelector/DateGenerator.h
@@ -9,10 +9,44 @@
 //          2017/10/4
 //===============================================================
 #pragma once
+#include <vector>
+#include "TimeSegment.h"
+
 class DateGenerator
 {
 
+    std::vector<std::string> m_tags;        //标签池
+    std::vector<TimeSegment> m_timeSegments;//空闲时间池
+
+    //************************************
+    // Method:    InitTags
+    // FullName:  DateGenerator::InitTags
+    // Access:    private 
+    // Returns:   void
+    // Qualifier: throw()
+    //************************************
+    void InitTags() throw();
+
+    //************************************
+    // Method:    InitTimeSegments
+    // FullName:  DateGenerator::InitTimeSegments
+    // Access:    private 
+    // Returns:   void
+    // Qualifier: throw()
+    //************************************
+    std::vector<TimeSegment> RandStudentFreeTimes() throw();
+
 public:
+
+    //************************************
+    // Method:    ChangeTags
+    // FullName:  DateGenerator::ChangeTags
+    // Access:    public 
+    // Returns:   void
+    // Qualifier: throw()
+    // Parameter: std::vector<std::string> tags
+    //************************************
+    void ChangeTags(std::vector<std::string> tags) throw();
 
     DateGenerator();
 
